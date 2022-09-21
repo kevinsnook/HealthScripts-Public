50f8acbe069f2f90dc5050e5ad9e27637b3298a8
kevinsnook <62143755+kevinsnook@users.noreply.github.com>
2022-09-21 12:00:41 +0100

    Update Test-WindowsHealth-cfg.ps1

--- Test-WindowsHealth-cfg.ps1
+++ Test-WindowsHealth-cfg.ps1
@@ -5,8 +5,7 @@ $MinimumUptime = 1
 #Number of days back before we alert about a cert being about to expire
 $CertificateTimeToAlert = 30
 #Fill in WIndows servers to monitor
-$targets = "ory1-eud-cvma01.eu.cobham.net","tnf1-eud-cvma01.eu.cobham.net","YUL1-NAD-CVMA11.na.cobham.net"
-#$targets = "ory1-eud-cvma01.eu.cobham.net","tnf1-eud-cvma01.eu.cobham.net"
+$targets = "server1.domain.net","server2.domain.net","server3.domain.net"
 #Minimum Percentage free on volumes before raising alert
 $volumePercentFree = 0.05
 #Maximum Percentage CPU usage before raising alert
@@ -16,15 +15,14 @@ $MemoryFreeUsagePercent = 10
 #Comma separated array of network interfaces to be ignored
 $IgnoreNetworkFailures = @("Embedded LOM 1 Port 4","Embedded LOM 1 Port 3","Embedded FlexibleLOM 1 Port 2","Embedded FlexibleLOM 1 Port 1")
 #Comma separated array of IP addresses to NOT ping (by default GreenScreen will ping all addresses on each server)
-$IgnoreIPaddresses = @("10.216.7.4","10.191.129.74","10.191.131.130","10.191.129.234","10.233.33.34")
+$IgnoreIPaddresses = @("11.216.7.4","10.121.139.74")
 #Comma separated array of server event numbers to ignore
-#$IgnoreServerEvents = @("10016")
+$IgnoreServerEvents = @("10016")
 #Comma separated array of servers to not log disk space
-#$IgnoreServerDiskSpace = @("boh2-eud-cvma01.eu.cobham.net","boh2-eud-cvma02.eu.cobham.net")
+$IgnoreServerDiskSpace = @("server1.domain.net","server2.domain.net","server3.domain.net")
 #Comma separated array of servers to not log hardware errors
-#$IgnoreHardwareErrors = @("boh2-eud-cvma01.eu.cobham.net","boh2-eud-cvma02.eu.cobham.net")
+$IgnoreHardwareErrors = @("server1.domain.net","server2.domain.net","server3.domain.net")
 #Services to monitor
-#$DesiredStateServices = @("GxBlr(Instance001)","GxClMgrS(Instance001)","GxCVD(Instance001)","GXMMM(Instance001)","GxFWD(Instance001)")
 $DesiredStateServices = @("GxClMgrS(Instance001)","GxCVD(Instance001)","GXMMM(Instance001)","GxFWD(Instance001)")
 
 #Report settings
@@ -35,9 +33,9 @@ $logfile = "C:\Source\Scripts\windows\windows_health.log"
 #Location of transcript file for script (shows command line output for script)
 $TranscriptFile = "C:\Source\Scripts\windows\windows_health_transcript.log"
 #Reportsubject = the heading for the html report
-$reportsubject = "Windows Health (CACm)"
+$reportsubject = "Windows Health"
 #Enter the URL of the report for use from the Web (this is the URL of the webpage this report will output)
-$ReportURL = "http://ORY1-EUD-VMAN01\Monitor\windowshealth.html"
+$ReportURL = "http://SERVER_WHERE_SCRIPT_IS_RUNNING\Monitor\windowshealth.html"
 #ReportMode - Set to $true to generate a HTML report. Uses the name of the Report file (see $ReportFile)
 $ReportMode=$true
 #ReportFile - name of file to output html report to
@@ -52,17 +50,14 @@ $AlertsOnly=$true
 #...................................
 
 #Send email to this address
-$recipients = "ITS.Datacentre.Services@cobham.com","CAC.IT.Notifications@cobham.com"
-#$recipients = "kevin.snook@cobham.com"
+$recipients = "someone@domain.com","someoneelse@domain.com"
 #Send email from this address
-$fromaddress = "CACm-Windows-Alerts@cobham.com"
+$fromaddress = "someone-Alerts@domain.com"
 #Send email using this relay host
-$smtpserver = "smtp.eu.cobham.net"
+$smtpserver = "smtp.domain.com"
 
 #Credentials
 #Credential for IPMI
-#$IPMICredential = Import-CliXml -Path c:\source\scripts\hpeloginonly.xml
-$IPMICredential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\hpeilo.xml
+$IPMICredential = Import-CliXml -Path c:\source\scripts\Credentials\hpeilo.xml
 #Credential for Servers
-#$Credential = Import-CliXml -Path c:\source\scripts\ks_cred.xml
-$Credential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\SVC-VMVRRW-001.xml
+$Credential = Import-CliXml -Path c:\source\scripts\Credentials\serveradmin.xml
