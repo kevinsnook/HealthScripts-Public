c332db192e5babb029d4d98bb8078e6d10dd0178
kevinsnook <62143755+kevinsnook@users.noreply.github.com>
2022-09-21 11:56:10 +0100

    Update Test-VMwareHealth-cfg.ps1

--- Test-VMwareHealth-cfg.ps1
+++ Test-VMwareHealth-cfg.ps1
@@ -8,8 +8,8 @@ $MaxHoursToScanLog = 12
 #Warn if uptime is below this number of hours
 $MinimumUptime = 2                                      	
 
-#Vcenter server to monitor
-$VCServer = "NHC0-EUD-VCM001.eu.cobham.net"
+#Vcenter server to monitor (will monitor all connected vCenters)
+$VCServer = "vcenter.domain.com"
 
 #Number of days back before we alert about a cert being about to expire
 $CertificateTimeToAlert = 20        				
@@ -25,10 +25,10 @@ $datastorePercentFree = 7
 $PartitionPercentFull = 90
 
 #Comma separated array of Hosts to ignore (if host has permananent known condition or is offline) (Please ensure this is exactly the same case and format as the hostname(s))
-$IgnoreHosts = @("poc1-inf-simp01.eu.cobham.net","poc1-inf-simp02.eu.cobham.net")
+$IgnoreHosts = @("server1.domain.net","server2.domain.net")
 
 #Comma separated array of VMs to ignore (if VM has permanent known condition or is offline) (Please ensure this is exactly the same case and format as the VM name(s))
-#$IgnoreVMs = @("test1.eu.cobham.net","test2.eu.cobham.net")
+$IgnoreVMs = @("server14.domain.net","server22.domain.net")
 
 #Comma separated array of VM alarms to ignore
 $IgnoreVMAlarms = @("Virtual machine memory usage","Virtual machine CPU usage")
@@ -37,13 +37,13 @@ $IgnoreVMAlarms = @("Virtual machine memory usage","Virtual machine CPU usage")
 $IgnoreHostAlarms = @("Virtual machine memory usage","Virtual machine CPU usage","Host memory usage","Host hardware power status")
 
 #Comma separated array of hosts with hardware errors to ignore
-#$IgnoreHardwareErrors = @("poc1-inf-simp01.eu.cobham.net","poc1-inf-simp02.eu.cobham.net")
+$IgnoreHardwareErrors = @("badserevr@domain.com","anotherserver@domain.com")
 
 #Run IPMI Checks
 $IPMIHardwareErrors = $true
 
 #Comma separated array of vCenter services to ignore
-#$IgnoreVCServices = @("updatemgr")
+$IgnoreVCServices = @("updatemgr")
 
 #Path to PuttyLink executable
 $PuttyLinkPath = "C:\PROGRA~1\PUTTY\plink.exe"
@@ -60,9 +60,9 @@ $logfile = "C:\Source\Scripts\vmware\VMware_health.log"
 #Location of transcript file for script (shows command line output for script
 $TranscriptFile = "C:\Source\Scripts\vmware\VMware_health_transcript.log"
 #Reportsubject = the heading for the html report
-$reportsubject = "VMware Health (UK HoldCo)"
+$reportsubject = "VMware Health"
 #Enter the URL of the report for use from the Web (this is the URL of the webpage this report will output)
-$ReportURL = "http://NHC0-EUD-VMAN01/Monitor/VMwareHealth.html" 
+$ReportURL = "http://SERVER_WHERE_SCRIPT_IS_RUNNING/Monitor/VMwareHealth.html" 
 #ReportMode - Set to $true to generate a HTML report. Uses the name of the Report file described in $ReportFile
 $ReportMode=$true
 #ReportFile - name of file to output html report to
@@ -78,11 +78,11 @@ $AlertsOnly=$true
 #...................................
 
 #Send email to this address
-$recipients = "ITS.DCI.Datacentre.Services@cobham.com"
+$recipients = "someone@domain.com"
 #Send email from this address
-$fromaddress = "HoldCo-VMware-Alerts@cobham.com"
+$fromaddress = "someone-Alerts@domain.com"
 #Send email using this relay host
-$smtpserver = "smtp.eu.cobham.net"
+$smtpserver = "smtp.domain.com"
 
 
 #...................................
@@ -90,16 +90,16 @@ $smtpserver = "smtp.eu.cobham.net"
 #....................................
 
 #VMware SSO Credential 
-$Credential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\admin@sso_nhc0.xml
+$Credential = Import-CliXml -Path c:\source\scripts\Credentials\admin@sso.xml
 #vCenter Credential
-$VCCredential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\SVC-VMVRRW-001-Backup.xml
+$VCCredential = Import-CliXml -Path c:\source\scripts\Credentials\vcenter_admin.xml
 #Monitoring credential on ESXi Hosts
-$ESXiMonitorCredential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\monitoring-user_cred.xml
+$ESXiMonitorCredential = Import-CliXml -Path c:\source\scripts\Credentials\monitoring-user_cred.xml
 #Root credential on vCenter
-$VCRootCredential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\vc_root_cred.xml
+$VCRootCredential = Import-CliXml -Path c:\source\scripts\Credentials\vc_root_cred.xml
 #HP ILO credential
-$ILOCredential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\hpeilo.xml
+$ILOCredential = Import-CliXml -Path c:\source\scripts\Credentials\hpeilo.xml
 #Dell iDRAC credential
-$iDRACCredential = Import-CliXml -Path c:\source\scripts\Credentials\SVC-VMVRRW-001\idrac_root.xml
+$iDRACCredential = Import-CliXml -Path c:\source\scripts\Credentials\idrac_root.xml
 
 
